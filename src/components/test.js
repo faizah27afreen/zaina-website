bce61c5f945189e699842a8ced20ce2e19e065e0
Faizah Afreen <faizah27afreen@gmail.com>
2020-08-17 18:04:25 -0400

    faizah

--- src/components/test.js
+++ src/components/test.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function test() {
-    <h1> just testing to see if this works </h1>
+    return (
+        <>
+            <h1> just testing to see if this works </h1>
+            <h2> faizah desktop </h2>
+        </>
+    )
 }
 
 export default test
